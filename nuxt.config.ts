4d8ebc6483f56b1bf325b24af0ac9a7b92f1fd73
m75943382-cloud <m75943382@gmail.com>
2026-03-18 00:16:34 +0200

    feat: add GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+
+  app: {
+    baseURL: '/test-nuxt/'
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
@@ -23,10 +28,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-01-15',
 
   nitro: {
+    output: {
+      publicDir: '.output/public'
+    },
     prerender: {
-      routes: [
-        '/'
-      ]
+      routes: ['/']
     }
   },
 
